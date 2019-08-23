326cde0b49bc3a5fed7e460759a3fc826ab800fc
Leonardo Monteiro <leonardo.monteiro@gvenergy.com.br>
2019-08-23 13:29:36 -0300

    Criação da model usuario e context.

---
+++ API/Models/Context.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API.Models
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
